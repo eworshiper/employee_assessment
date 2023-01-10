877bad1a4e097a9743c835a2852646254a3b3820
eworshiper <purba.evenetus2@gmail.com>
2023-01-11 00:36:50 +0700

    penambahan data dan model untuk halaman team

---
+++ lib/model/team_model.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class Team {
+  final String nama;
+  final String role;
+  final String backImage;
+  Team({
+    required this.nama,
+    required this.role,
+    required this.backImage,
+  });
+}
