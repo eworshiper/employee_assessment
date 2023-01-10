46cbc6863e52a6e130f1a1c1c4ca5a478feda60b
eworshiper <purba.evenetus2@gmail.com>
2023-01-11 00:37:07 +0700

    pembuatan halaman team

---
+++ lib/screens/team_screen.dart
@@ -0,0 +1,64 @@
+import 'package:dashboard/widgets/team_list.dart';
+import 'package:flutter/material.dart';
+
+import '../constant.dart';
+import 'components/side_menu.dart';
+
+class TeamScreen extends StatelessWidget {
+  const TeamScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        iconTheme: const IconThemeData(color: Colors.grey, size: 28),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+              color: Colors.grey,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.notifications,
+              color: Colors.grey,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 5, right: 16, bottom: 5),
+            child: const CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=60"),
+            ),
+          )
+        ],
+      ),
+      drawer: const SideMenu(),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: const [
+              Text("My Team", style: TextStyle(color: kDarkBlue, fontSize: 26, fontWeight: FontWeight.bold)),
+              SizedBox(
+                height: 10,
+              ),
+              TeamList(),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
